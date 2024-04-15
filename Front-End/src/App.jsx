27109067b809f66e40f2875aa7ff79d3c6016eba
SahilK28 <110329160+SahilK28@users.noreply.github.com>
2024-04-15 17:05:35 +1200

    Updates on Front-End AClinicianSearch

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AdminLogin } from "./pages/AdminLogin";
 import Landing from "./pages/Landing";
+import {AdminClinicianSearch} from "./pages/AClinicianSearch"
 
 function App() {
   console.log("Rendering App component");
