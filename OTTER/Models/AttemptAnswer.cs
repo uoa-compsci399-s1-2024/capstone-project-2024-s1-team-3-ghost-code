ff121c276ef5ca668a2dee67df2be0ea4048d691
Angus Wright <angus@wrightfamily.nz>
2024-04-17 14:42:14 +1200

    Update AttemptAnswer.cs model to include composite key

--- OTTER/Models/AttemptAnswer.cs
+++ OTTER/Models/AttemptAnswer.cs
@@ -1,15 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OTTER.Models
 {
+    [PrimaryKey(nameof(Question), nameof(Answer))]
     public class AttemptAnswer
     {
-        [Key]
-        public string AttemptAID { get; set; }
-        [ForeignKey("Question"), Column(Order = 0)]
         public int Question { get; set; }
-        [ForeignKey("Answer"), Column(Order = 1)]
         public int Answer { get; set; }
     }
 }
