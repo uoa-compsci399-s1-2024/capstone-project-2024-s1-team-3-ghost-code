ac02eb683da9f4de4dec53e4cf5a6e1c901a333b
Candy <yumcandyxd@gmail.com>
2024-05-20 09:21:54 +1200

    added loading page from angus template
    inserted and converted angus' html into page.

--- src/pages/PracQuiz.jsx
+++ src/pages/PracQuiz.jsx
@@ -283,7 +283,24 @@ const storeSelectedAnswersForQuestion = (selectedAnswers, questionIndex) => {
   const addLeadingZero = (number) => (number > 9 ? number : `0${number}`);
 
   if (questions.length === 0) {
-    return <div>Loading...</div>;
+    return (<div className='loadingpage'>
+      <head><title>404 - Page Not Found</title></head>
+      <body>
+        <div className='loadingbody'>
+          <div className='loadingtextcont'>
+            <h1 className='loadingh1'>404 - Page Not Found</h1>
+            <p className='loadingp'>The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</p>
+
+              <div className="cont-return-but">
+                <Link to="/quizDashboard" style={{ textDecoration: "none" }}>
+                  <button className="btn return-button">Back to Modules</button>
+                </Link>
+              </div>
+              
+          </div>
+        </div>
+      </body>
+    </div>);
   }
 
  
