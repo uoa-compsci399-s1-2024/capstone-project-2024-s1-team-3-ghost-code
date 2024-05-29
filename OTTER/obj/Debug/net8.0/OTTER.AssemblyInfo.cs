69c2b1cf4b99f4f6dded751e7b6e63a4de944640
SahilK28 <110329160+SahilK28@users.noreply.github.com>
2024-05-29 16:39:36 +1200

    Updating how users are Certified to button and also PreSurvey override

--- OTTER/obj/Debug/net8.0/OTTER.AssemblyInfo.cs
+++ OTTER/obj/Debug/net8.0/OTTER.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OTTER")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+339389965a67544055981ac7973b2dbbea757189")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+242a7d0faf61acf75fa01aaec23abf56f543b803")]
 [assembly: System.Reflection.AssemblyProductAttribute("OTTER")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OTTER")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
